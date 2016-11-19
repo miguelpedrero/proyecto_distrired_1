e77e93dcd566668bce49faa429d40bad499c3fd0
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-11-18 21:54:38 -0300

    inicio de sesion

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :assign_default_role
+
+  def assign_default_role
+  	self.role = 'cliente' if self.role.nil?
+  end
+end
