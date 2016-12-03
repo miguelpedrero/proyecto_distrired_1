9074502a77880121f7a7e4d3392e9a874b1c0137
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-12-02 22:11:34 -0300

    index usuario

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 before_action :configure_sign_up_params, only: [:create] 
 # before_action :configure_account_update_params, only: [:update]
 
+ # def new
+#   @rol = [["Distribuidor"], ["Proveedor"], ["Administrador"], ["Cliente"]]
+# end
+
   # GET /resource/sign_up
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+
+  resources :users
   devise_for :users, controllers: { registrations: 'users/registrations'}
   root to: "products#index"
 
