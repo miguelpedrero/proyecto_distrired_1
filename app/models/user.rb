56376c814b88ab4c6a51caab27f0f982ef8022c6
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-12-03 00:47:58 -0300

    User Create y View Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-belongs_to :commune
+  belongs_to :commune
 
   before_create :assign_default_role
+  before_create :verify_user_password_per_role
 
   def assign_default_role
   	self.role = 'cliente' if self.role.nil?
   end
+
+  def verify_user_password_per_role
+  	unless self.role == 'Cliente'
+  		self.password = Devise.friendly_password
+  	end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :products
 
-  resources :users
   devise_for :users, controllers: { registrations: 'users/registrations'}
+  
+  resources :users , only: [:index, :new, :edit, :create, :update, :destroy, :show]
   root to: "products#index"
 
 end
